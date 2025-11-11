7115b03f72ab8240418fb059b410420b0505058e
malakelsadek <malak.elsadek@student.giu-uni.de>
2025-11-11 09:47:32 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -6,15 +6,13 @@ using namespace std;
 
 //#write Person class here
 
-
-
-
-
-
-
-
-
-
-
+class Person{
+private:
+    string name;
+    int id;
+public:
+    //Person(string n, int i);
+    void display();
+};
 
 #endif
